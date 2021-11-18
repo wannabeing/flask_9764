d2c571fcfcddc74a3e8e9b8fd3b88202e201dbfb
choihyuk <wannabeing@naver.com>
2021-11-18 17:59:17 +0900

    ocr 페이지 구현했당 ㅎㅎ 관련 오류는 velog에 올리기!

--- views/file_views.py
+++ views/file_views.py
@@ -1,5 +1,5 @@
 from blog import *
-from flask import Blueprint, request, send_file
+from flask import Blueprint, request, send_file, url_for
 from werkzeug.utils import secure_filename
 from views.login_views import login_required
 import os
@@ -29,7 +29,8 @@ def upload_file():
         file = request.files['file'] # POST 방식으로 업로드페이지에서 넘어오면 file이라는 이름의 폼으로 전송된 파일을 가져옴
         file.save('./uploads/'+secure_filename(file.filename)) # 파일이름을 보호하고 file.save로 파일 객체를 지정한 폴더에 저장한다
         file_list = os.listdir("./uploads")
-        return render_template('ftp/ftp_check.html', file_list=file_list)
+        test = 'C:/projects/blogproject/static/ocr_images/'+(file.filename)
+        return render_template('ftp/ftp_check.html', file_list=test)
 
 
 # 다운로드 HTML 렌더링
--- views/ocr_views.py
+++ views/ocr_views.py
@@ -1,9 +1,8 @@
 import os.path
-
 from flask import Blueprint, render_template, request, url_for
 from views.login_views import login_required
 from werkzeug.utils import redirect
-
+from werkzeug.utils import secure_filename
 import ocr
 
 bp = Blueprint('ocr', __name__, url_prefix='/ocr')
@@ -16,12 +15,23 @@ def main_page():
     return render_template('ocr/ocr_main.html')
 
 
-@bp.route('/check/')
+@bp.route('/check/', methods=['POST', 'GET'])
 @login_required
 def check():
-    path ='C:/projects/blogproject'+ url_for('static', filename='ocr_images/hdg.jpg')
+    if request.method == 'POST':
+        file = request.files["file"]
+        file.save('./static/ocr_img/'+secure_filename(file.filename))
+        path = 'C:/projects/blogproject/static/ocr_img/'+file.filename
+        output = ocr.kakao_ocr(path, appkey).json()
+        return render_template('ocr/ocr_check.html', real=output)
+
+@bp.route('/test/')
+@login_required
+def test():
+    path = 'C:/projects/blogproject/static/ocr_img/hdg_2.jpg'
     resize = ocr.kakao_ocr_resize(path)
     if resize is not None:
         path = resize
+
     output = ocr.kakao_ocr(path, appkey).json()
-    return render_template('ocr/ocr_check.html', real=output)
+    return render_template('ocr/ocr_test.html', real=output)
