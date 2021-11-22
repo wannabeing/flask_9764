733c7aa7f8ab9c99d98cd9f3b321cfe497412f4a
choihyuk <wannabeing@naver.com>
2021-11-22 17:48:02 +0900

    OCR 구현완료! 정상적으로 이미지를 텍스트로 변환한다.

--- views/ocr_views.py
+++ views/ocr_views.py
@@ -1,4 +1,4 @@
-import os.path
+import json
 from flask import Blueprint, render_template, request, url_for
 from views.login_views import login_required
 from werkzeug.utils import redirect
@@ -22,16 +22,15 @@ def check():
         file = request.files["file"]
         file.save('./static/ocr_img/'+secure_filename(file.filename))
         path = 'C:/projects/blogproject/static/ocr_img/'+file.filename
-        output = ocr.kakao_ocr(path, appkey).json()
-        return render_template('ocr/ocr_check.html', real=output)
-
-@bp.route('/test/')
-@login_required
-def test():
-    path = 'C:/projects/blogproject/static/ocr_img/hdg_2.jpg'
-    resize = ocr.kakao_ocr_resize(path)
-    if resize is not None:
-        path = resize
-
-    output = ocr.kakao_ocr(path, appkey).json()
-    return render_template('ocr/ocr_test.html', real=output)
+        output = ocr.kakao_ocr(path, appkey).json() #dic 구조인거 같음 (list)
+        dict = output.values()
+        test = output['result'][0]['recognition_words'][0]
+        # outputdata는 JSON 문자열임/ dumps()는 Python객체를 JSON문자열로 변환
+        outputdata = json.dumps(output, ensure_ascii=False, sort_keys=True)
+        array = json.loads(outputdata)
+        ex = len(array['result'])
+        print = []
+        for i in range(len(array['result'])):
+            print.append(array['result'][i]['recognition_words'][0])
+        str = ' '.join(print)
+        return render_template('ocr/ocr_test.html', real=str)
